d915a566cb8d46cf598d0f9db04aa6aed86862c0
JaredLarios <lar21038@byui.edu>
2025-04-04 08:05:32 -0600

    first commit

---
+++ src/js/utils.mjs
@@ -0,0 +1,21 @@
+export function renderWithTemplate(template, parent, data, callback) {
+    parent.insertAdjacentHTML("afterbegin", template);
+    callback && callback(data);
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const tempalte = await response.text();
+  return tempalte;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("partials/header.html");
+  const footerTemplate = await loadTemplate("partials/footer.html");
+
+  const headerElement = document.querySelector("header");
+  const footerElement = document.querySelector("footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
