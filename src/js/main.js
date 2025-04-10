c1ed0b69bbbe3f00331f023cbb02f16e26b0e411
JaredLarios <lar21038@byui.edu>
2025-04-10 08:23:25 -0600

    refactor: remove unused counter setup code and clean up main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,5 @@
-import { setupCounter } from "./counter.js";
 import { loadHeaderFooter } from "./utils.mjs";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <div class="card">
-      <button id="counter-button" type="button">Count numbers</button>
-      <p class="counter"></p>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-
-const counterButton = document.querySelector("#counter-button");
-const counterText = document.querySelector(".counter");
-
-
-setupCounter(counterButton, counterText);
 
 loadHeaderFooter();
 
