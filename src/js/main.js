801bd60dd5f4fdd3debc100e5cf841892df3890f
JaredLarios <lar21038@byui.edu>
2025-04-10 19:55:27 -0600

    feat: update hero section text and button for daily photo navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
+const button = document.querySelector("#daily-photo");
+
 loadHeaderFooter();
+
+button.addEventListener("click", () => location.replace("/daily/"))
